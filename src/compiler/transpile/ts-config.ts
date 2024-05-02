5bb15f6027b1e97e30f94cb3b1d627b251f68c5f
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2024-05-02 10:18:44 -0400

    well that was embaressing

--- src/compiler/transpile/ts-config.ts
+++ src/compiler/transpile/ts-config.ts
@@ -21,19 +21,49 @@ import type * as d from '../../declarations';
  * @returns an object containing TypeScript compiler options
  */
 export const getTsOptionsToExtend = (config: d.ValidatedConfig): ts.CompilerOptions => {
+  console.log(`compilerOptions is '${JSON.stringify(config.tsCompilerOptions, null, 2)}`);
+  // enum ModuleKind {
+  //   None = 0,
+  //   CommonJS = 1,
+  //   AMD = 2,
+  //   UMD = 3,
+  //   System = 4,
+  //   ES2015 = 5,
+  //   ES2020 = 6,
+  //   ES2022 = 7,
+  //   ESNext = 99,
+  //   Node16 = 100,
+  //   NodeNext = 199,
+  //   Preserve = 200,
+  // }
+  console.log(`Detected Module '${(config.tsCompilerOptions as ts.CompilerOptions)?.module}'`);
+  // enum ModuleResolutionKind {
+  //   Classic = 1,
+  //   /**
+  //    * @deprecated
+  //    * `NodeJs` was renamed to `Node10` to better reflect the version of Node that it targets.
+  //    * Use the new name or consider switching to a modern module resolution target.
+  //    */
+  //   NodeJs = 2,
+  //   Node10 = 2,
+  //   Node16 = 3,
+  //   NodeNext = 99,
+  //   Bundler = 100,
+  // }
+  console.log(`Detected Module Resolution '${(config.tsCompilerOptions as ts.CompilerOptions)?.moduleResolution}'`);
   const tsOptions: ts.CompilerOptions = {
     experimentalDecorators: true,
     // if the `DIST_TYPES` output target is present then we'd like to emit
     // declaration files
     declaration: config.outputTargets.some(isOutputTargetDistTypes),
-    module: ts.ModuleKind.Node16,
+    module: (config.tsCompilerOptions as ts.CompilerOptions)?.module ?? ts.ModuleKind.Node16,
     moduleResolution:
-      ((config.tsconfig as any)?.compilerOptions as ts.CompilerOptions)?.moduleResolution ??
-      ts.ModuleResolutionKind.Node16,
+      (config.tsCompilerOptions as ts.CompilerOptions)?.moduleResolution ?? ts.ModuleResolutionKind.Node16,
     noEmitOnError: false,
     outDir: config.cacheDir || config.sys.tmpDirSync(),
     sourceMap: config.sourceMap,
     inlineSources: config.sourceMap,
   };
+  console.log(`Final opts: ${JSON.stringify(tsOptions, null, 2)}`);
   return tsOptions;
 };
